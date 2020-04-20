81a4b4c8ea401346055daee2829ef4e4bb05b05a
leigharobinson <lmarobinson24@gmail.com>
2020-04-20 14:44:04 -0500

    Completed this Practice assignment

---
+++ overly-excited.js
@@ -0,0 +1,17 @@
+// For this exercise, you need to create a single JavaScript function named addExcitement
+//  that will use console.log() to print out a sentence to the browser console.
+// let addExcitement = "I am so excited to learn Java Script!";
+// console.log(addExcitement);
+
+let arraySentanceWords = ["I ", "am ", "so ", "excited ", "to ", "learn ", "Java ", "Script", "!",];
+let newVar = arraySentanceWords 
+
+
+
+var res = "";
+for (i = 0; i < arraySentanceWords.length; i++) {
+        //   console.log(arraySentanceWords[i]);
+        res = res.concat(arraySentanceWords[i]);
+        console.log(res);
+         }
+
